d2c98b171000b6bc5f0f8f41126834fd92456b32
dumas45 <alexei.skorodumov@gmail.com>
2025-05-27 16:36:07 +0300

    Add short lines file

--- chess/lines2.py
+++ chess/lines2.py
@@ -3,10 +3,11 @@ import re
 
 INPUT_PATH = '/home/alex/data/chess/lines.pgn'
 OUTPUT_PATH = '/home/alex/data/chess/lines-flat.pgn'
+OUTPUT_PATH_SHORT = '/home/alex/data/chess/lines-short.pgn'
 
 
 def get_letter(s: str) -> str:
-    vocab = 'barceiona'
+    vocab = 'BARCELONA'
     h = hashlib.new('sm3', s.encode())
     idx = sum(h.digest()[-4:]) % len(vocab)
     return vocab[idx]
@@ -14,15 +15,16 @@ def get_letter(s: str) -> str:
 
 def get_bar_code(text_line):
     moves = re.findall(r'\S+', text_line)
-    bar = []
+    letters = []
+    step = 4
     i = 0
-    while i < 5:
-        parts = moves[i * 4:(i + 1) * 4]
+    while i < len(moves) // step:
+        parts = moves[i * step:(i + 1) * step]
         i += 1
         if parts:
-            bar.append(get_letter(''.join(parts)))
+            letters.append(get_letter(''.join(parts)))
 
-    return ''.join(bar) or '0'
+    return ''.join(letters) or '0'
 
 
 def form_line(stack):
@@ -37,13 +39,24 @@ def form_line(stack):
     return ' '.join(moves)
 
 
-def process(headers: dict[str, str], lines: list[str], is_first: bool) -> None:
-    if not lines:
-        return
+def get_moves_part(line: str) -> str:
+    m = re.search(r'(?P<moves>((\s+)?(\d+)?\w+\S?)+)\s*(?P<suffix>.*)?', line)
+    return m.group('moves')
 
-    print()
-    for k, v in headers.items():
-        print(k, v)
+
+def trim_line(line: str) -> str:
+    m = re.search(r'(?P<moves>((\s+)?(\d+)?\w+\S?)+)\s*(?P<suffix>.*)?', line)
+    parts = re.findall(r'\S+', m.group('moves'))
+    trim = 24 if len(parts) % 2 == 0 else 23
+    parts = parts[:trim]
+    suffix = m.group('suffix')
+    if suffix:
+        parts.append(suffix)
+
+    return ' '.join(parts)
+
+
+def prepare_variation_text(lines: list[str]) -> str:
     text = ' '.join(lines)
     text = re.sub(r'\{[^}]*}', '', text)
     text = re.sub(r'\d+\.{3}', '', text)
@@ -51,8 +64,10 @@ def process(headers: dict[str, str], lines: list[str], is_first: bool) -> None:
     text = re.sub(r'\.\s+', '.', text)
     text = re.sub(r'\(\s+', '(', text)
     text = re.sub(r'\s+\)', ')', text)
-    print(text)
+    return text
 
+
+def traverse_variation_text(text: str) -> list[str]:
     res = []
     stack = [[]]
     it = re.finditer(r'([^()]*)([()]?)', text)
@@ -72,22 +87,49 @@ def process(headers: dict[str, str], lines: list[str], is_first: bool) -> None:
         line = form_line(stack)
         res.append(line)
 
+    return res
+
+
+def process(headers: dict[str, str], sep_lines: list[str], is_first: bool) -> None:
+    if not sep_lines:
+        return
+
+    text = prepare_variation_text(sep_lines)
+    lines = traverse_variation_text(text)
+    short_lines = set()
+
     if is_first:
         mode = 'w'
     else:
         mode = 'a'
 
-    with open(OUTPUT_PATH, mode) as f:
-        for i, text_line in enumerate(res):
-            print(text_line)
+    with open(OUTPUT_PATH, mode, encoding='UTF-8') as f:
+        for text_line in sorted(lines):
+            bar_code = get_bar_code(get_moves_part(text_line))
+
+            short_line = trim_line(text_line)
+            short_lines.add(short_line)
+
             _headers = dict(headers)
-            # _headers['White'] = '%s %02d' % (headers.get('White'), i + 1)
-            _headers['White'] = '%s %s' % (headers.get('White'), get_bar_code(text_line))
+            _headers['White'] = f'{headers.get('White')} {bar_code}'
             for k, v in _headers.items():
-                f.write('[%s "%s"]\n' % (k, v))
+                f.write(f'[{k} "{v}"]\n')
 
             f.write('\n')
             f.write(text_line)
+            f.write('\n\n')
+
+    with open(OUTPUT_PATH_SHORT, mode, encoding='UTF-8') as f:
+        for short_line in sorted(short_lines):
+            bar_code = get_bar_code(get_moves_part(short_line))
+
+            _headers = dict(headers)
+            _headers['White'] = f'{headers.get('White')} {bar_code}'
+            for k, v in _headers.items():
+                f.write(f'[{k} "{v}"]\n')
+
+            f.write('\n')
+            f.write(short_line)
             f.write('\n\n')
 
 
@@ -95,7 +137,7 @@ def main() -> None:
     headers = {}
     lines = []
     is_first = True
-    with open(INPUT_PATH, 'r') as input_file:
+    with open(INPUT_PATH, 'r', encoding='UTF-8') as input_file:
         while True:
             line = input_file.readline()
             if not line:
